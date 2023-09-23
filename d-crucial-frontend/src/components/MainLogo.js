483d0685425d32ca66b31eedd63e75641f4bcc25
David Alméciga <wdalmeciga@rcnradio.com.co>
2023-09-23 16:05:00 -0500

    fix crucial logo

--- d-crucial-frontend/src/components/MainLogo.js
+++ d-crucial-frontend/src/components/MainLogo.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const MainLogo = () => {
     return <Image
-        src="/d-crucial-logo.png"
+        src="/d-crucial-logo.svg"
         alt="The Daily Crucial"
         width={253 * 2}
         height={63 * 2}
