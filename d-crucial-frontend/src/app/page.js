df8df1532583254258b76200924cee7dbdcc816d
David Alméciga <wdalmeciga@rcnradio.com.co>
2023-09-23 12:56:29 -0500

    add advertisement

--- d-crucial-frontend/src/app/page.js
+++ d-crucial-frontend/src/app/page.js
@@ -16,7 +16,7 @@ export default async function Home() {
   const data = pageData[0]
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pl-10 pr-10 pt-4 pb-4">
+    <main className="flex max-w-[1263px] min-h-screen flex-col items-center justify-between pl-10 pr-10 pt-4 pb-4">
       <div className="mb-4">
         <MainLogo />
       </div>
